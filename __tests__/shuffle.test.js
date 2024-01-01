11ac6b8e6dbe591a701ac30053c7431681a6357b
Shattuck Kuzmic <shattuckkuzmic@gmail.com>
2024-01-01 11:02:35 -0800

    unit tests

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,5 +1,11 @@
 const shuffle = require("../src/shuffle");
 
 describe("shuffle should...", () => {
-  // CODE HERE
+
+  it("checks that all the same items are in the array", () =>{
+    expect(shuffle(array)).toContain(array)})
+
+  it("checks that array length remains the same after shuffling", () => {
+      expect(shuffle(array.length)).toHaveLength(array.length)
+  })
 });
